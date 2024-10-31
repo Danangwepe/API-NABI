5482b2e1034770ec0b07c8cccf55595747561be8
Danang Wahyu <danangprasektiyo15@gmail.com>
2024-10-31 14:41:19 +0700

    add path

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 // Endpoint untuk mengambil data dari data.json dengan query parameter
 app.get('/info', (req, res) => {
     try {
-        const rawData = fs.readFileSync('./data.json');
+        const rawData = fs.readFileSync(path.resolve(__dirname, 'data.json'));
         const data = JSON.parse(rawData);
 
-        // Mengecek apakah ada query parameter tertentu
         const { field } = req.query;
-
         if (field) {
-            // Jika field yang diminta ada di data, kembalikan field tersebut
             if (data.hasOwnProperty(field)) {
                 res.json({ [field]: data[field] });
             } else {
                 res.status(404).json({ message: `Field '${field}' tidak ditemukan dalam data.` });
             }
         } else {
-            // Jika tidak ada query parameter, kembalikan seluruh data
             res.json(data);
         }
     } catch (error) {
@@ -30,13 +27,10 @@ app.get('/info', (req, res) => {
 
 app.get('/info/:id', (req, res) => {
     try {
-        const rawData = fs.readFileSync('./data.json');
+        const rawData = fs.readFileSync(path.resolve(__dirname, 'data.json'));
         const data = JSON.parse(rawData);
 
-        // Mengambil id dari parameter URL
         const id = parseInt(req.params.id);
-
-        // Memeriksa apakah id valid
         if (id >= 0 && id < data.length) {
             res.json(data[id]);
         } else {
